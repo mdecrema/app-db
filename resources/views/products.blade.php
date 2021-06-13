92bce5c91fba7132bcee58bba8daa77575244860
Marco De Crema <marcodecrema@libero.it>
2021-06-13 22:29:59 +0200

    img storage

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -13,8 +13,8 @@
 <div class="col-lg-10 offset-lg-1">
 @foreach($products as $product)
 <a href="products/{{$product['id']}}">
-    <div class="col-lg-4" style="border: 1px solid blue; padding: 10px; overflow: hidden;">
-        <img src="{{ $product->photo1) }}" alt="" style="width: 100%; height: 100%">
+    <div class="col-lg-4" style="padding: 10px; overflow: hidden;">
+        <img src="{{ $product->photo1 }}" alt="" style="width: 100%; height: 100%">
     </div>
 </a>
 @endforeach
