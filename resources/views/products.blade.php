a8394866bd2c1f47569f71a88bd6ffd440e8097d
Marco De Crema <marcodecrema@libero.it>
2021-06-14 20:04:49 +0200

    test 2

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -15,7 +15,7 @@
 
 <a href="products/{{$product['id']}}">
     <div class="col-lg-4" style="height: 200px; padding: 10px; overflow: hidden; border: 1px solid blue">
-        <img src="{{ url('storage/'.$product->photo1) }}" alt="" style="width: 100%; height: 100%">
+        <img src="{{ URL::asset('storage/'.$product->photo1) }}" alt="" style="width: 100%; height: 100%">
     </div>
 </a>
 @endforeach
