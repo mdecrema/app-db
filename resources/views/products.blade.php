7dc7cb389ef3c15adea6d16ac7b9768a2cdf4072
Marco De Crema <marcodecrema@libero.it>
2021-06-26 11:06:57 +0200

    padding descrizione prodotti

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -17,13 +17,13 @@
 <a href="products/{{$product['id']}}">
     <div class="col-lg-4 col-xs-6 col-sm-6 col-md-6" style="height: 400px; padding: 10px; overflow: hidden; position: relative">
         <img class="active" src="{{'https://img-space.fra1.digitaloceanspaces.com/img-space/uploads/images/'.$product->photo1}}" alt="" style="width: 320px; height: 320px">
-        <div style="width: 100%; height: 80px; position: absolute; bottom: 0; left: 0; padding: 10px">
+        <div style="width: 100%; height: 80px; position: absolute; bottom: 0; left: 0; padding: 25px 10px">
         <h5 style="color: #000; font-weight: bold">{{ $product->nome }}</h5>
         <span style="color: #000;">{{ $product->amount }} €</span>
         </div>
         {{-- expand icon --}}
         <div style="width: 30px; height: 30px; position: absolute; top: 0; right: 0; text-align: center; padding: 5px">
-        <i class="fas fa-expand-alt" style="color: darkgrey; font-size: 20px"></i>
+            <i class="fas fa-expand-alt" style="color: darkgrey; font-size: 20px"></i>
         </div>
     </div>
 </a>
