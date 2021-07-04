87d53ddd2369a7dc88e3a2f330a36cfb8d9c4a60
Marco De Crema <marcodecrema@libero.it>
2021-07-04 19:02:04 +0200

    fix 3

--- resources/views/tees.blade.php
+++ resources/views/tees.blade.php
@@ -13,7 +13,7 @@
     @foreach($tees as $tee)
         <div class="col-lg-4 col-xs-6 col-sm-6" style="width: 46%; height: 250px; margin: 0 2%; padding: 10px; overflow: hidden; position: relative; margin-bottom: 30px; margin: auto; display: inline-block;">
         <a href="products/{{$tee['id']}}" style="float: left;">
-            <img src="{{'https://img-space.fra1.digitaloceanspaces.com/img-space/uploads/images/'.$tee->photo1}}" alt="" style="width: 100%; height: 150px;">
+            <img  class="active" src="{{'https://img-space.fra1.digitaloceanspaces.com/img-space/uploads/images/'.$tee->photo1}}" alt="" style="width: 100%; height: 150px;">
             <div style="width: 100%; height: 100px; position: absolute; bottom: 0; left: 0; padding: 15px 25px;">
                 <span style="color: #000; font-size: 14px; font-weight: bold">{{ $tee->nome }}</span><br>
                 <span style="color: grey; font-size: 11px;">{{ $tee->categoria }} - {{ $tee->genere }}</span><br>
