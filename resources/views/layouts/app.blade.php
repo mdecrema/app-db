9d09e750986c31217dd5fa6e81c47a12e017e28f
Marco De Crema <marcodecrema@libero.it>
2021-12-19 23:34:11 +0100

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -140,7 +140,7 @@
 
         </footer>
 
-    <script>
+    <script type="text/javascript">
         /* Set the width of the side navigation to 250px */
         function openNav() {
             document.getElementById("mySidenav").style.width = "250px";
