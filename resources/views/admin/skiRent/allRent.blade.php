abfaf198a6607e0838a25dd27a3c10c84abf9b58
Marco De Crema <marcodecrema@libero.it>
2022-01-11 21:40:57 +0100

    fix4

--- resources/views/admin/skiRent/allRent.blade.php
+++ resources/views/admin/skiRent/allRent.blade.php
@@ -52,6 +52,11 @@
 
 <!-- Errore chiamata -->
 <div>
+    <form action="{{ route('admin.skiRent.statusChange', 1) }}" method="post">
+        @csrf
+        @method("POST")
+        <button onclick="provaAjax()">click ajax</button>
+    </form>
     <h4>ERRORE CHIAMATA:</h4>
     <strong id="error" style="font-size: 20px">ff</strong>
 </div>
@@ -64,9 +69,52 @@
 </div>
 
 
+  <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          ...
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
 
 
 <script>
+
+function provaAjax() {
+    let resp = 1;
+
+    $.ajax({
+        headers: {
+                    'X-CSRF-Token': token 
+               },
+                  url: 'allRent/scancode/'+resp,
+                  method: 'POST',
+                  data: {
+                     id: 1,
+                  },
+                  success: function(result){
+                        
+                        console.log(result);
+                  },
+                  error: function(request,error){
+                      console.log(request);
+                      document.getElementById('error').innerHTML=JSON.stringify(error);
+                  }
+                 });
+}
 
 var html5QrcodeScanner = new Html5QrcodeScanner(
 	"reader", { fps: 10, qrbox: 250 });
@@ -80,8 +128,9 @@ function onScanSuccess(decodedText, decodedResult) {
     html5QrcodeScanner.clear();
     // ^ this will stop the scanner (video feed) and clear the scan area.
     //
+    $('#exampleModalCenter').modal('show');
    
-               $.ajax({
+              /* $.ajax({
                   url: 'admin/dashboard/skiRent/allRent/scancode',
                   method: 'POST',
                   data: {
@@ -92,10 +141,11 @@ function onScanSuccess(decodedText, decodedResult) {
                   },
                   error: function(request,error){
                       document.getElementById('error').innerHTML=JSON.stringify(error);
+
                   }
                  });
             
-
+                 */
 }
 
 html5QrcodeScanner.render(onScanSuccess);
