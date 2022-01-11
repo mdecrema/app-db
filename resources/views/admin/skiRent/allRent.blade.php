bcfc1d352d36b152465b923f33183c94c620f1d6
decremasarce <decrema@sarce.it>
2022-01-11 17:30:47 +0100

    barcodes box

--- resources/views/admin/skiRent/allRent.blade.php
+++ resources/views/admin/skiRent/allRent.blade.php
@@ -41,7 +41,8 @@
 <div style="margin: 20px 0">
     @foreach($skis as $ski)
     <div style="margin: 50px 0">
-        <img src="data:image/png;base64,{{DNS1D::getBarcodePNG($ski->id, 'C39')}}" alt="barcode" />
+        <!--<img src="data:image/png;base64,{{DNS1D::getBarcodePNG($ski->id, 'C39')}}" alt="barcode" />-->
+        {!!  DNS1D::getBarcodeHTML("$ski->id", "C39")!!}
     </div>
     <span>{{ $ski->brand }}</span><br />
     <span style="color: red">{{ $ski->status }}</span>
