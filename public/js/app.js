efdbfbb5458802eb263dbff8648d997455a88c54
Marco De Crema <marcodecrema@libero.it>
2022-01-21 16:07:01 +0100

    adjustment3

--- public/js/app.js
+++ public/js/app.js
@@ -37429,8 +37429,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\MarcoDeCrema\progetti\app-db\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\MarcoDeCrema\progetti\app-db\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Progetti\app-db\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Progetti\app-db\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
