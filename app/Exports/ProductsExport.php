82d67ff3df2498d2d6f844b5245743c1dc7c7e8e
Marco De Crema <m.decrema@custom.it>
2022-05-18 16:58:20 +0200

    productsExport with Headings

--- app/Exports/ProductsExport.php
+++ app/Exports/ProductsExport.php
@@ -4,8 +4,9 @@ namespace App\Exports;
 
 use App\Product;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class ProductsExport implements FromCollection
+class ProductsExport implements FromCollection, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -13,5 +14,13 @@ class ProductsExport implements FromCollection
     public function collection()
     {
         return Product::all();
+    }
+
+    public function headings(): array
+    {
+        return [
+            'nome', 'photo1', 'photo2', 'photo3', 'photo4', 'photo5', 'categoria', 'genere', 
+            'description', 'colore', 'sizes', 'counterSizeType', 'brand', 'amount', 'availability', 'valutazione', 'appView', 'created_at', 'updated_at'
+        ];
     }
 }
