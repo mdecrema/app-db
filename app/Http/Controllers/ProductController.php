6f70e4fdcde5ed66da259c3eb6e1f675775ae01e
Marco De Crema <m.decrema@custom.it>
2022-06-06 14:37:37 +0200

    fix error

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\DB;
 use App\Product;
 use App\Item;
 use App\Imports\ProductsImport;
